1fc98601489eda53847a4d9970e8a011686707db
AadityaJain2009 <81620807+AadityaJain2009@users.noreply.github.com>
2021-12-27 15:13:14 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+
+function roa(){
+    window.location = "ro1.html";
+}
+
+function rob(){
+    window.location = "ro2.html";
+}
+
+function roc(){
+    window.location = "ro3.html";
+}
+
+function rod(){
+    window.location = "ro4.html";
+}
+
+function roe(){
+    window.location = "ro5.html";
+}
+
+function back(){
+    window.location = "index.html";
+}
+
